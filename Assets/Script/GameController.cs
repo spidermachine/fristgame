c3787b860a1645c104b16eba4ff0bddff5da2f7c
zkp <zkp1985@gmail.com>
2019-02-23 02:02:31 +0800

    init

---
+++ Assets/Script/GameController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+
+    private float speedUp = 0f;
+
+    private float speedHorizontal = 0f;
+
+    public static GameController instance;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+       if (instance == null) {
+            instance = this;
+         }else if (instance != this) {
+            Destroy(gameObject);
+         }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        speedHorizontal = Input.GetAxis("Horizontal") * Time.deltaTime * 10;
+        speedUp = Input.GetAxis("Vertical") * Time.deltaTime * 10;
+        if (speedUp < 0) speedUp = 0f;
+    }
+
+    public float getSpeedUp() {
+        return this.speedUp;
+    }
+
+    public float getSpeedHorizontal() {
+        return this.speedHorizontal;
+    }
+}
---
+++ Assets/Script/ScrollObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollObject : MonoBehaviour
+{
+
+    private Rigidbody2D rd2d;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rd2d = GetComponent<Rigidbody2D>();
+        rd2d.velocity = Vector2.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rd2d.velocity = Vector2.down * GameController.instance.getSpeedUp() * 10; 
+    }
+}
